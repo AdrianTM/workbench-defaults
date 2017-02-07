87b35492a2130376e68f6b57be6111444312384e
Adrian <adrian@mxlinux.org>
2017-02-07 11:54:48 -0500

    added cli programs in apt-install script

--- scripts/apt-install.sh
+++ scripts/apt-install.sh
@@ -1,5 +1,9 @@
 #/bin/bash
-apt-get install clonezilla partclone qt4-fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 geany ceni wireshark zenmap 
+apt-get install clonezilla partclone qt4-fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
+geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath \
+dvtm telnet rkhunter chkrootkit
+
+
 
 
 
