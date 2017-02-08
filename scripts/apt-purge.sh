aeb8ef49275c3cd1a42431350ac21b015a58112e
Adrian <adrian@mxlinux.org>
2017-02-08 11:04:30 -0500

    small adjustments

--- scripts/apt-purge.sh
+++ scripts/apt-purge.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-apt-get purge mx-clocky luckybackup peg-e chromium-bsu gimp shotwell gimp-data thunderbird gscan2pdf \
+sudo apt-get purge mx-clocky luckybackup peg-e chromium-bsu gimp shotwell gimp-data thunderbird gscan2pdf \
 asunder clementine pdfshuffler dconf-cli libalut0 libconfig-general-perl libcrypto++9 libechonest2.1 \
 libexiv2-13 libextutils-depends-perl libextutils-pkgconfig-perl libfilesys-df-perl libfont-ttf-perl \
 libftgl2 libgee-0.8-2 libgexiv2-2 libglc0 libglewmx1.10 libgoo-canvas-perl libgoocanvas-common \
@@ -10,5 +10,4 @@ libproc-processtable-perl libprojectm2 libprotobuf9 libqjson0 libqxt-core0 libqx
 libreadonly-perl libsane-perl libset-intspan-perl libtiff-tools libtry-tiny-perl libwebkit2gtk-4.0-37 \
 pkg-config python-poppler python-pypdf python-rsvg shotwell-common smtube libamd2.3.1 libbabl-0.1-0 \
 libcamd2.3.1 libccolamd2.8.0 libcholmod2.1.2 libgegl-0.2-0 libgimp2.0 libjavascriptcoregtk-1.0-0 libopenraw1 \
-libwebkitgtk-1.0-0 libwmf0.2-7 pepperflashplugin-nonfree
-
+libwebkitgtk-1.0-0 libwmf0.2-7 bunsen-pepperflash
