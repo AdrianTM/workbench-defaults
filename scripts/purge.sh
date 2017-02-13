ed3e1da96be0b3823d88f2968504d1138c1e499e
Adrian <adrian@mxlinux.org>
2017-02-13 14:41:25 -0500

    added clamav clamtk

--- scripts/purge.sh
+++ scripts/purge.sh
@@ -10,7 +10,7 @@ libproc-processtable-perl libprojectm2 libprotobuf9 libqjson0 libqxt-core0 libqx
 libreadonly-perl libsane-perl libset-intspan-perl libtiff-tools libtry-tiny-perl libwebkit2gtk-4.0-37 \
 pkg-config python-poppler python-pypdf python-rsvg shotwell-common smtube libamd2.3.1 libbabl-0.1-0 \
 libcamd2.3.1 libccolamd2.8.0 libcholmod2.1.2 libgegl-0.2-0 libgimp2.0 libjavascriptcoregtk-1.0-0 libopenraw1 \
-libwebkitgtk-1.0-0 libwmf0.2-7 bunsen-pepperflash papirus-gtk-icon-theme mediterranean-thce-themes \
+libwebkitgtk-1.0-0 libwmf0.2-7 bunsen-pepperflash papirus-gtk-icon-theme mediterranean-themes \
 mx-bluebird-themes mx-greybird-themes mx-sound-theme-borealis tango-icon-theme zukitwo-themes mx-system-sounds \
-mx-apps mx-defaultlook mx-welcome
+mx-apps mx-defaultlook mx-welcome mx-adwaita-dark-xfce-themes
 sudo apt-get autoremove
