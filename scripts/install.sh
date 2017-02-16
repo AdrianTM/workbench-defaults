ea1f245ff8db4eeae8e5896dd309fa0329d2144d
Adrian <adrian@mxlinux.org>
2017-02-16 12:42:24 -0500

    remove duplicate

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 sudo apt-get install clonezilla partclone qt4-fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
 geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath \
-dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd dcfldd w3m lynx vim
+dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim
 
 
 
