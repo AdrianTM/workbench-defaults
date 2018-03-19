d02ddf782f2084573b6d60456435cf4851b26064
Adrian <adrian@mxlinux.org>
2018-03-19 11:29:10 -0400

    adjust removal/addition

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-sudo apt-get install clonezilla partclone qt4-fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
+sudo apt-get install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
 geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath \
-dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest
+dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest abiword
 
 
 
