429b96c1723aa9e54c64e47a14fe1d28e554c5a7
Adrian <adrian@mxlinux.org>
2018-03-27 10:09:42 -0400

    add gvfs-backends

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 sudo apt-get -y install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
 geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath \
-dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest abiword linssid
+dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest abiword linssid gvfs-backends
 
 
 
