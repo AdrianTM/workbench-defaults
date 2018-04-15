9bee5e813fd1a96f335972dd2ff0c4f6fcb495cb
Adrian <adrian@mxlinux.org>
2018-04-15 14:38:19 -0400

    add ncdu

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo apt-get -y install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
-geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath \
+geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath ncdu \
 dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest abiword linssid gvfs-backends
 
 
