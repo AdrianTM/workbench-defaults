43f13600fba534f7a1988cce96cb60d3eee581d0
AdrianTM <AdrianTM@users.noreply.github.com>
2018-08-16 19:36:14 -0400

    Update install.sh
    added attr and rename recommended packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-sudo apt-get -y install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 \
+sudo apt-get -y install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 attr rename \
 geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath ncdu \
 dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest abiword linssid gvfs-backends
 
