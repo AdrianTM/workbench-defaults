c93e18ab8b387870572427d29fc674b218c2d93c
Adrian <adrian@mxlinux.org>
2019-03-04 16:49:00 -0500

    remove xscreensaver; add light-locker

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 sudo apt-get -y install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 attr rename mx-boot-options\
 geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath ncdu \
-dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ stressapptest abiword linssid gvfs-backends
+dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ \
+stressapptest abiword linssid gvfs-backends light-locker
 
 
 
