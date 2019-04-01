850b550d46a0d5653d13199bb8cf5cddf43b38d8
Adrian <adrian@mxlinux.org>
2019-04-01 16:37:48 -0400

    add memtester

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 sudo apt-get -y install clonezilla partclone fsarchiver nwipe freefilesync grsync gftp-gtk blockout2 attr rename mx-boot-options \
-geany ceni wireshark zenmap chntpw nmap meld fping screen iputils-arping iputils-ping iputils-tracepath ncdu \
+geany ceni wireshark zenmap chntpw nmap meld memtester fping screen iputils-arping iputils-ping iputils-tracepath ncdu \
 dvtm telnet rkhunter chkrootkit dtrx clamav clamtk dcfldd w3m lynx vim ghex wakeonlan iperf dmraid fio bonnie++ \
 stressapptest abiword linssid gvfs-backends light-locker iotop
 
